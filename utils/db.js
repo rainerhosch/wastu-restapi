6c94dedcd75d6a17ceb34da950ad7fe9417688c1
rizkyoktan <rizkyoktan@wastukancana.ac.id>
2025-11-03 14:23:35 +0700

    Add secondary database connection for DEWA environment
    - Introduced a new MySQL connection pool for the DEWA environment, allowing for separate database configurations.
    - Updated the module export to include both the primary and DEWA connection pools for improved database management.

--- utils/db.js
+++ utils/db.js
@@ -8,5 +8,12 @@ const pool = mysql.createPool({
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
 });
+const pool_dewa = mysql.createPool({
+    host: process.env.DB_HOST_DEWA,
+    port: process.env.DB_PORT_DEWA || 3306,
+    user: process.env.DB_USER_DEWA,
+    password: process.env.DB_PASS_DEWA,
+    database: process.env.DB_NAME_DEWA,
+});
 
-module.exports = pool;
+module.exports = { pool, pool_dewa };
